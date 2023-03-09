56a6603f47d33db90c13cb5fed1dd13f96d85ef8
Binayak Jha <jha36binayak@gmail.com>
2023-03-09 20:36:39 +0545

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,21 +4,4 @@ import 'firebase/compat/firestore';
 
 
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyBr_KqF_rgQM2QUxSioKWgtPGCJtJZdHUM",
-    authDomain: "linkedin-clone-4d6ea.firebaseapp.com",
-    projectId: "linkedin-clone-4d6ea",
-    storageBucket: "linkedin-clone-4d6ea.appspot.com",
-    messagingSenderId: "375590836527",
-    appId: "1:375590836527:web:c6cb8caca169a8281569da",
-    measurementId: "G-XXSGQ1B04N"
-};
-
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-
-const db = firebaseApp.firestore();
-
-const auth = firebase.auth();
-
-export { db, auth };
+// put your own firebase config
